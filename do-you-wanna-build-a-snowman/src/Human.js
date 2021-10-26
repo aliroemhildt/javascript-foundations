941845c34c7c63cf822cda1879f47a9d2d191aba
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-25 23:13:45 -0600

    Add human class, wip

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,31 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+  constructor(name) {
+    this.name = name;
+    this.wantsToBuildASnowman = true;
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+  }
+  gatherMaterials(type, amount) {
+    if (type === "carrots") {
+      this.materials.carrots = amount;
+    } else if (type === "buttons") {
+      this.materials.buttons = amount;
+    } else if (type === "coal") {
+      this.materials.coal += amount;
+    }
+  }
+  buildASnowman() {
+    snowman.coal = this.materials.coal;
+    snowman.carrots = this.materials.carrots;
+    snowman.snowballs = this.materials.snowballs;
+    snowman.buttons = this.materials.buttons;
+  }
 }
 
 module.exports = Human;
