51b3a197c942fd788c2715c7405cf5bb40a08a4c
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-25 23:29:25 -0600

    Complete vampire class

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,19 @@
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    this.pet = pet || 'bat';
+    this.thirsty = true;
+    this.ouncesDrank = 0;
+  }
+
+  drink() {
+    if (this.ouncesDrank >= 50) {
+      return 'I\'m too full to drink anymore!';
+    } else {
+      this.thirsty = false;
+      this.ouncesDrank += 10;
+    };
+  };
+};
+
+module.exports = Vampire;
