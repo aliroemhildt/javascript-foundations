682719c323a2cab876dae19546aa4d7a102494d8
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-27 12:32:44 -0600

    Create Medusa class

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,15 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  };
+
+  gazeAtVictim(person) {
+    this.statues.push(new Statue(person.name));
+  };
+};
+
+module.exports = Medusa; Person;
