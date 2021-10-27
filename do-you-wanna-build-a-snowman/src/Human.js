30ca5ff821a464b71e23f7e8ccd9c24b3d01c77f
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-26 21:36:31 -0600

    Complete human class

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -9,23 +9,49 @@ class Human {
       coal: 0,
       buttons: 0,
       carrots: 0
-    }
-  }
+    };
+  };
+
   gatherMaterials(type, amount) {
     if (type === "carrots") {
-      this.materials.carrots = amount;
+      this.materials.carrots += amount;
     } else if (type === "buttons") {
-      this.materials.buttons = amount;
+      this.materials.buttons += amount;
     } else if (type === "coal") {
       this.materials.coal += amount;
-    }
-  }
+    } else if (type === "snowballs") {
+      this.materials.snowballs += amount;
+    };
+  };
+
   buildASnowman() {
-    snowman.coal = this.materials.coal;
-    snowman.carrots = this.materials.carrots;
-    snowman.snowballs = this.materials.snowballs;
-    snowman.buttons = this.materials.buttons;
-  }
-}
+    return new Snowman({
+      carrots: this.materials.carrots,
+      coal: this.materials.coal,
+      buttons: this.materials.buttons,
+      snowballs: this.materials.snowballs
+    });
+    //   var snowmanMaterials = {
+    //     coal: this.materials.coal,
+    //     carrots: this.materials.carrots,
+    //     snowballs: this.materials.snowballs,
+    //     buttons: this.materials.buttons
+    //   }
+    //   var snowman = new Snowman(snowmanMaterials);
+    // };
+  };
+
+  placeMagicHat() {
+    if (this.materials.coal >= 2 && this.materials.buttons >= 5 && this.materials.carrots >= 1 && this.materials.snowballs >= 2) {
+      return "Woah, this snowman is coming to life!";
+    } else {
+      return "I guess I didn't build it correctly.";
+    };
+  };
+};
+
+
+
+
 
 module.exports = Human;
