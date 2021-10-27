7000ee42314596bffcef07ea2ad01fbbee283feb
Ali Roemhildt <aliroemhildt@gmail.com>
2021-10-26 22:22:35 -0600

    Complete magician class

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -3,6 +3,7 @@ class Magician {
     this.name = `The Great ${magician.name}`;
     this.assistant = magician.assistant;
     this.favoriteAccessory = magician.clothing || 'top hat';
+    this.confidencePercentage = 10;
   };
 
   performIncantation(spell) {
@@ -10,10 +11,19 @@ class Magician {
   };
 
   performTrick(){
+    this.confidencePercentage += 10;
     if (this.favoriteAccessory === 'top hat') {
       return 'PULL RABBIT FROM TOP HAT';
     } else {
       return 'PULL DOVE FROM SLEEVE';
+    };
+  };
+
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant) {
+      return "WOW! The magician totally just sawed that person in half!";
+    } else {
+      return "Oh no, this trick is not ready!";
     };
   };
 };
