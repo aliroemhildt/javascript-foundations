26e90574e3da9a03b89b93f8d15d870e9e70ed43
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-01 22:54:20 -0600

    Refactor method

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -8,20 +8,31 @@ class Medusa {
   };
 
   gazeAtVictim(person) {
-    var freedPerson;
-    if (this.statues.length === 3) {
-      var freedStatue = this.statues[0];
-      freedPerson = new Person(freedStatue.name);
+    this.statues.push(new Statue(person.name));
+    if (this.statues.length > 3) {
+      var freedPerson = new Person(this.statues[0].name);
       freedPerson.mood = 'relieved';
       this.statues.shift();
-    };
-    if (this.statues.length < 3) {
-      this.statues.push(new Statue(person.name));
-    };
-    if (freedPerson) {
       return freedPerson;
     };
   };
 };
+
+
+//     var freedPerson;
+//     if (this.statues.length === 3) {
+//       var freedStatue = this.statues[0];
+//       freedPerson = new Person(freedStatue.name);
+//       freedPerson.mood = 'relieved';
+//       this.statues.shift();
+//     };
+//     if (this.statues.length < 3) {
+//       this.statues.push(new Statue(person.name));
+//     };
+//     if (freedPerson) {
+//       return freedPerson;
+//     };
+//   };
+// };
 
 module.exports = Medusa;
