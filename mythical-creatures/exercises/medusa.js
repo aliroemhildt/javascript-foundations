8c360ef7ec6c3c775dd51ef7d62da4f8e5827bb9
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-01 18:15:06 -0600

    Complete medusa class

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -8,10 +8,20 @@ class Medusa {
   };
 
   gazeAtVictim(person) {
+    var freedPerson;
+    if (this.statues.length === 3) {
+      var freedStatue = this.statues[0];
+      freedPerson = new Person(freedStatue.name);
+      freedPerson.mood = 'relieved';
+      this.statues.shift();
+    };
     if (this.statues.length < 3) {
       this.statues.push(new Statue(person.name));
+    };
+    if (freedPerson) {
+      return freedPerson;
     };
   };
 };
 
-module.exports = Medusa; Person;
+module.exports = Medusa;
--- mythical-creatures/test/medusa-test.js
+++ mythical-creatures/test/medusa-test.js
@@ -78,6 +78,7 @@ describe('Medusa', function() {
     var victim3 = new Person('Scott');
     var victim4 = new Person('Bob');
 
+
     var statue1 = new Statue('Pam');
     var statue2 = new Statue('Khalid');
     var statue3 = new Statue('Scott');
@@ -96,7 +97,7 @@ describe('Medusa', function() {
     assert.deepEqual(medusa.statues, [statue2, statue3, statue4]);
   });
 
-  it.skip('should return a freed statue to Person form', function() {
+  it('should return a freed statue to Person form', function() {
     var medusa = new Medusa('Hannah');
     var victim1 = new Person('Pam');
     var victim2 = new Person('Khalid');
@@ -113,7 +114,7 @@ describe('Medusa', function() {
     assert.equal(person.name, 'Pam');
   });
 
-  it.skip('should be relieved when returned to Person form', function () {
+  it('should be relieved when returned to Person form', function () {
     var medusa = new Medusa('Pam');
     var victim1 = new Person('Travis');
     var victim2 = new Person('Kayla');
