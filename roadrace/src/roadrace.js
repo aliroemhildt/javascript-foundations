265171623919ca12f86e50bb5f84e696a7f96377
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-06 11:06:12 -0600

    Complete runner and roadrace classes

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,28 @@
 class Roadrace {
+  constructor(raceInfo) {
+    this.name = raceInfo.title;
+    this.location = raceInfo.city;
+    this.distance = 0;
+    this.participants = [];
+  };
 
-}
+  setDistance(raceDistance) {
+    this.distance = raceDistance;
+    return `The ${this.name} in ${this.location} is a ${this.distance} mile race.`;
+  };
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  };
+
+  completeRace() {
+    // loop through participants of that race
+    // run race (arguments)
+
+    for (var i = 0; i < this.participants.length; i++){
+      this.participants[i].runRace(this.name, this.distance);
+    };
+  };
+};
 
 module.exports = Roadrace;
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,25 @@
 class Runner {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  };
 
-}
+  runSomeMiles(distance) {
+    this.milesRun += distance;
+    this.fitness += distance;
+  };
+
+  stretch() {
+    this.fitness += 0.5;
+  };
+
+  runRace(raceName, distance) {
+    this.completedRaces.push(raceName);
+    this.runSomeMiles(distance);
+  };
+};
 
 module.exports = Runner;
