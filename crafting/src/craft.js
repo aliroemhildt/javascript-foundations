9daf45cf7443f4307b52d154d294de04465fe85a
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-06 10:40:38 -0600

    Complete material and craft classes

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,29 @@
 class Craft {
+  constructor(craftInfo) {
+    this.name = craftInfo.type;
+    this.materials = craftInfo.materials
+    this.completed = false;
+  };
 
-}
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  };
+
+  calculateProjectTotal() {
+    // create totalCost variable, set to 0
+    // loop through materials array
+    // - for each material, calculate material cost
+    // - add the cost for that material to the totalCost
+    // return the totalCost
+
+    var totalCost = 0;
+    for (var i = 0; i <this.materials.length; i++) {
+      var materialCost = this.materials[i].calculateMaterialCost();
+      totalCost += materialCost;
+    };
+    return totalCost;
+  };
+};
 
 module.exports = Craft;
