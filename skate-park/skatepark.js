b18ab3e84d288ea14ed589989a54db93ea5776ec
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-08 22:31:04 -0700

    Complete skatepark and skater class

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -8,6 +8,21 @@ class SkatePark {
     this.cost = parkInfo.price || 0;
     this.occupants = [];
   };
+
+  admit(skater) {
+    if (this.occupants.length < 3 && this.isPrivate && skater.money < this.cost) {
+      return `Sorry, you don't have enough money.`;
+    } else if (this.occupants.length < 3 && this.isPrivate && skater.money >= this.cost) {
+        this.occupants.push(skater);
+        skater.money -= this.cost;
+        return `Welcome to ${this.name}, the cost will be $${this.cost}.00.`;
+    } else if (this.occupants.length < 3 && !this.isPrivate) {
+        this.occupants.push(skater);
+      return `Welcome to the free ${this.name} Skatepark!`;
+    } else if (this.occupants.length === 3) {
+      return `Sorry, we are at max capacity. Thank you for understanding.`;
+    };
+  };
 };
 
 module.exports = SkatePark;
