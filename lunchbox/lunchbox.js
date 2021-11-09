c66c750760b0a80cf68ad633259c20049c483a4d
Ali Roemhildt <aliroemhildt@gmail.com>
2021-11-08 22:54:50 -0700

    Complete lunchbox and snack classes

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,28 @@
 class LunchBox {
+  constructor(info) {
+    this.owner = info.owner;
+    this.material = info.madeOf;
+    this.shape = info.shape;
+    this.color = info.color;
+    this.snacks = [];
+  };
 
-}
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  };
+
+  findHealthySnacks() {
+    var healthySnacks = [];
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].checkForHealthy()) {
+        healthySnacks.push(this.snacks[i].type);
+      };
+    };
+
+    return healthySnacks;
+  };
+  
+};
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,27 @@
 class Snack {
+  constructor(name) {
+    this.deliciousLevel = 'extra';
+    this.type = name;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  };
 
-}
+  getEaten() {
+    this.amount -= 10;
+    if ((this.amount / 100) <= 0.2) {
+      this.cuttingItClose = true;
+    };
+  };
+
+  checkForHealthy() {
+    if (this.type.toLowerCase().includes('fruit')) {
+      return true;
+    } else {
+      return false;
+    };
+  };
+  
+};
 
 module.exports = Snack;
